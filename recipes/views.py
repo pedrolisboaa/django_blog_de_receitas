7c68979033690d1f43e1765567ffde45b0835b12
pedrolisboaa <phlisboa2000@gmail.com>
2022-09-20 16:15:31 -0300

    Criando o Header

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Essa é minha home')
-
-
-def sobre(request):
-    return HttpResponse('Esse é meu sobre')
-
-
-def contato(request):
-    return HttpResponse('Essa é meu contatos')
+    return render(request, 'recipes/pages/home.html')
