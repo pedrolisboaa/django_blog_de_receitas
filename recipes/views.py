47a80eca7b73986eefff59fab641a5bb797c57be
pedrolisboaa <phlisboa2000@gmail.com>
2022-09-22 00:00:32 -0300

    Atualizando os blocos

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe_view.html')
