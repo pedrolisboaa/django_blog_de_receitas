803186a1d22bf1961e84144e25f10e47f9dc3559
pedrolisboaa <phlisboa2000@gmail.com>
2022-09-22 23:41:32 -0300

    Terminando o Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(12)]
+    })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe_view.html')
+    return render(request, 'recipes/pages/recipe_view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
