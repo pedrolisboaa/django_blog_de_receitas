6373a6f2d050b5166e2596fdf811b30cfa7f90aa
pedrolisboaa <phlisboa2000@gmail.com>
2022-09-23 20:39:17 -0300

    Criando os  models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
+from ast import mod
+from distutils.command.upload import upload
+from statistics import mode
+from turtle import update
 from django.db import models
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.CharField(max_length=175)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=70)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=75)
+    preparation_steps = models.TextField()
+    preparaation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(uploada_to='recipes/cover/%Y/%m/%d/')
+
+
